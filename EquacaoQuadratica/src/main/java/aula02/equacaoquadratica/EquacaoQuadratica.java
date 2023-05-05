170ff572a90c14ca4923bed1a027fe7a8884f394
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-05-05 19:17:10 -0300

    .

---
+++ EquacaoQuadratica/src/main/java/aula02/equacaoquadratica/EquacaoQuadratica.java
@@ -0,0 +1,25 @@
+
+package aula02.equacaoquadratica;
+import java.util.*;
+public class EquacaoQuadratica {
+
+    public static void main(String[] args) {
+        /*Declaração de variáveis*/
+        double a, b, c;
+        Scanner le = new Scanner(System.in);
+        System.out.print("Informe o valor de |a|:");
+        
+        /*Preenchimento de variáveis*/
+        a = le.nextDouble();
+        System.out.print("Informe o valor de |b|:");
+        b = le.nextDouble();
+        System.out.print("Informe o valor de |c|:");
+        c = le.nextDouble();
+        
+        double Delta = (b * b) - (4 * a * c);
+        double x1 = (- b + Math.sqrt(Delta)) / (2 * a);
+        double x2 = (- b - Math.sqrt(Delta)) / (2 * a);
+        /*Saída de dados*/
+        System.out.println("Raizes: " + x1 + " e " + x2);
+    }
+}
