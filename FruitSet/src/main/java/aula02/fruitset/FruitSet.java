8c0340ad8857e810bf479b40eeb7955b687941ba
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-05-05 19:57:32 -0300

    Update FruitSet.java

--- FruitSet/src/main/java/aula02/fruitset/FruitSet.java
+++ FruitSet/src/main/java/aula02/fruitset/FruitSet.java
@@ -1,6 +1,7 @@
-
 package aula02.fruitset;
+
 import java.util.*;/*Para trabalhar com coleções, entrada de dados(scanner) e componentes de data e hora*/
+
 class Macas{
     /*Atributos*/
    String tipo;
@@ -24,8 +25,9 @@ class Macas{
        System.out.print("Peso(em gramas): ");
        peso = le.nextInt();
    }
-}
    
+   
+}
 
 public class FruitSet {
 
