cc5785fe1e0c84c3e96f83869a30ce5a33328905
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-05-19 20:49:31 -0300

    .

--- TesteAnimais/src/main/java/aula09/testeanimais/TesteAnimais.java
+++ TesteAnimais/src/main/java/aula09/testeanimais/TesteAnimais.java
@@ -1,22 +1,26 @@
 
 package aula09.testeanimais;
 
+//criação da classe abstrata
 abstract class Animal {
+    //método abstrato
     public abstract void emitirSom();
 }
 
+
 class Cachorro extends Animal {
     public void emitirSom() {
-        System.out.println("O cachorro faz 'Au au!'");
+        System.out.println("'Au au! é o som que o cachorro emite.'");
     }
 }
 
 class Gato extends Animal {
     public void emitirSom() {
-        System.out.println("O gato faz 'Miau!'");
+        System.out.println("'Miau!' é o som que o gato emite.");
     }
 }
-
+//classe pública que que instancia
+//objetos de cada uma das subclasses e chama o método
 public class TesteAnimais {
 
     public static void main(String[] args) {
--- TesteShapes/src/main/java/aula09ex02/testeshapes/TesteShapes.java
+++ TesteShapes/src/main/java/aula09ex02/testeshapes/TesteShapes.java
@@ -1,16 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package aula09ex02.testeshapes;
 
-/**
- *
- * @author nathaliabrazkrawczyk
- */
-public class TesteShapes {
+//classe abstrata
+abstract class Shape {
+    public abstract double calcularÁrea();
+}
 
+//subclasse
+class Retângulo extends Shape {
+    private double comprimento;
+    private double largura;
+
+    public Retângulo(double comprimento, double largura) {
+        this.comprimento = comprimento;
+        this.largura = largura;
+    }
+
+    public double calcularÁrea() {
+        return comprimento * largura;
+    }
+}
+
+//subclasse
+class Círculo extends Shape {
+    private double raio;
+
+    public Círculo(double raio) {
+        this.raio = raio;
+    }
+
+    //método
+    public double calcularÁrea() {
+        return Math.PI * raio * raio;
+    }
+}
+//classe que instancia objetos de cada uma das subclasses e chama o método
+public class TesteShapes {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Retângulo retângulo = new Retângulo(6, 9);
+        Círculo círculo = new Círculo(10);
+
+        double áreaRetângulo = retângulo.calcularÁrea();
+        double áreaCírculo = círculo.calcularÁrea();
+
+        System.out.println(" *-*-*-*-*- Áreas -*-*-*-*-* \n");
+        System.out.println("Área do Retângulo: " + áreaRetângulo);
+        System.out.println("Área do Círculo: " + áreaCírculo);
     }
 }
